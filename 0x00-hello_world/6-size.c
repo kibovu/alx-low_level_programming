49c9af367a1cb9cfa0aa13c708e64b6f50376ed6
kibovu <mutahigikonyo@gmail.com>
2023-10-05 06:39:41 -0700

    c is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(b));
-	printf("Size of an int: %zu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("size of a float: %zu byte(s)\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("size of a float: %lu byte(s)\n", sizeof(e));
 
 	return (0);
 }
