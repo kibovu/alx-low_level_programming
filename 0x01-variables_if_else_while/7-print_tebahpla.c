e1d0c4a450f5a3426d2e2da092e180b59e2956af
kibovu <mutahigikonyo@gmail.com>
2023-10-06 15:07:56 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *
+  *Return:0
+  */
+int main(void)
+
+{
+	int a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
