4c01de3d17127592786431d9e23363d9c74de60f
kibovu <mutahigikonyo@gmail.com>
2023-10-06 14:56:19 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *
+  *Return:0
+  */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
