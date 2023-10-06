beb01e3b3634472cf09337a2621a05e3d6b6c2e0
kibovu <mutahigikonyo@gmail.com>
2023-10-06 09:25:28 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Return:0
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+
+}
