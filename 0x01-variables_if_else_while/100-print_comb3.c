12c6ece138375e3bc762794192aaec58c85dceca
kibovu <mutahigikonyo@gmail.com>
2023-10-06 17:02:37 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *
+  *Return:0
+  */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+				if (a >=  b)
+				{
+					continue;
+					b++;
+				}
+
+				putchar(a);
+				putchar(b);
+				if (a == 56 && b == 57)
+				{
+					continue;
+				}
+
+				putchar('a');
+				putchar('b');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
