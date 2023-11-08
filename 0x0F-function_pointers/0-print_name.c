2326906a8d55a1da21e98b13d99e1dc6cd06052f
kibovu <mutahigikonyo@gmail.com>
2023-11-08 11:07:12 -0800

    second commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - prints a string arguement
+  * print_name - prints string arguement
   *@name: string
-  *@f: pointer to a function
-  *
+  *@f: pointer to  function
+  *function pointers
   */
 
 void print_name(char *name, void (*f)(char *))
