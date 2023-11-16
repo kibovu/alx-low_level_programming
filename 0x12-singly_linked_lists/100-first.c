bfb0e848de391c301277cb7c82ba74c7fa203f96
kibovu <mutahigikonyo@gmail.com>
2023-11-16 13:29:47 -0800

    hello

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * printFirst - prints a string
+  *
+  */
+
+void printFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
